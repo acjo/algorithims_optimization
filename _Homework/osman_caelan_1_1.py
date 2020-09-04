d6be3ea9b6b5d99a74a8c936e2a6610998908bc2
Caelan Osman <caelano@byu.edu>
2020-09-03 19:03:52 -0600

    homework 1.2

--- _Homework/osman_caelan_1_1.py
+++ _Homework/osman_caelan_1_1.py
@@ -11,29 +11,35 @@ if __name__ == "__main__":
         else:
             listB = delta * [0] + listB
 
+        #2n+1-delta ~2n
         # set initial i value for looping
         i = len(listA) - 1
+        #+3
+        #log_2(n-1)
 
         #loop through the lists and find the difference between coresponding elements
-        while i >= 0:
-            if listA[i] - listB[i] < 0: #if the corresponding element listA is smaller than in listB we need to carry so execute
-                if i == 0:
+        while i >= 0: #~n
+            if listA[i] - listB[i] < 0: #if the corresponding element listA is smaller than in listB we need to carry so execute 4n-4 ~4n
+                if i == 0: #n-1 ~n
                     #in the case that number represented by listA is smaller than the one represented by
                     #ListB we want to make sure we capture the negative.
-                    listA[i] -= listB[i]
-                else:
+                    listA[i] -= listB[i] #4
+                else: #n-2
                     #This is "carrying the one" operation
-                    listA[i-1] -= 1
+                    listA[i-1] -= 1 #4(n-2) ~4n
                     #subtract 1 from the next element
-                    listA[i] += 10
+                    listA[i] += 10 #3(n-2) ~3n
                     #add 10 to the current element
-                    listA[i] -= listB[i]
+                    listA[i] -= listB[i] #4(n-2) ~ 4n
                     #subtract the elements
             else:
                 #if the element at listA is not smaller than the corresponding element of listB then just subtract the elements
-                listA[i] = listA[i] - listB[i]
+                listA[i] -= listB[i]
             #increment our loop variable
-            i -= 1
+            i -= 1 #2(n-2) ~2n
         return listA
 
-#O(n) is both the temporal and spatial complexity. See paper homework for details on explanation. 
+#O(n) is both the temporal and spatial complexity. See paper homework for details on explanation.
+#2n + n + 4n + n + 4n + 3n + 4n + 2n = 21n
+#temporal complexity 21N
+#Spatial complexity ~2n
---
+++ _Homework/osman_caelan_1_2.py
@@ -0,0 +1,33 @@
+'''This file includes the algorithms for questions 1.13 and 1.14'''
+
+if __name__ == "__main__":
+    def SmallestElementIndex(L):
+        min_val = L[0] #2
+        min_val_index = 0 #1
+        n = len(L) #2
+        i = 1 #1
+        while i < n: # hit n times executes n-1 times ~n
+            if L[i] < min_val: #hit n-1 times 2 primitive operations ~2n (worst case enters every time)
+                min_val = L[i] #2 primitive operations n-1 times ~2n
+                min_val_index = i #1 operation ~n
+            i +=1 #2 operations n-1 ~2n
+        return min_val_index
+
+    #leading order temporal complexity is ~8n
+    #We store n, i, min_val_index, min_val which are all bounded by log_2(#digits) the List L has
+    #is ~n so it the leading order spatial complexity is ~n
+
+    def SelectionSort(L):
+        n = len(L) - 1 #3
+        i = 0#1
+        while i < n: #checked n times failes once executes n-1 times ~n
+            index = SmallestElementIndex(L[i:]) + i#call a function with leading order
+            #temporal complexity ~8n n-1 times so this is ~8n^2
+            temp = L[i]#2
+            L[i] = L[index] #2
+            L[index] = temp #1
+            i += 1 #2 (n-1) times ~2n
+        return L
+
+#leading order temporal complexity 8n^2
+#leading order spatial complexity n^2
