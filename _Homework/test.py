e8ad1ebf77d09deda0f152bd793ab0938cead253
Caelan Osman <caelano@byu.edu>
2020-09-13 13:29:16 -0600

    updating test file

--- _Homework/test.py
+++ _Homework/test.py
@@ -6,3 +6,15 @@ import random as rand
 u = np.random.randint(0, 10, (3, 1))
 v = np.random.randint(0, 10, (3, 1))
 print(v @ u.T)
+
+Top:
+  u * v^T flops: (2*2^n-1) spits out some number ~2^(n+1)
+  I + (some number) flops: 2^2n ~ 2^2n
+  (matrix) * x flops: 2**n(2*2**n - 1) spits out another vector ~2^(2n+1)
+  total: 2^(n+1)-1 + 2^(2n) + 2^(2n+1) - 2^n  leading order temporal complexity ~ 2^(2n) + 2^(2n+1 )
+  
+  bottom:
+  v^T*x flops: (2*2^n-1) spits out some number ~2^(n + 1)
+  some number * u flops: 2^n spits out another vector ~ ~2^n
+  x + (some number * u) flops: 2^n ~2^n
+  total: 2^(n+2) -2  ~leading order temporal complexity ~ 2^(n+2)
