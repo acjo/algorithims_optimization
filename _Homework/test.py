0425114f49948c81cc9b82af15ca98e0f7b43ed1
Caelan Osman <caelano@byu.edu>
2020-11-04 20:50:40 -0700

    testing

--- _Homework/test.py
+++ _Homework/test.py
@@ -1,28 +1,7 @@
 import numpy as np
 import random as rand
 
+A = np.array([[1, 0.5, 1/3.], [0, 0, 1/3.], [0., 0.5, 1/3.]])
 
-def count(string):
-    return len(string)
+print(np.allclose(A.sum(axis=0), np.ones(A.shape[1])))
 
-def characters(string):
-    char_set = set()
-    for character in string:
-        char_set.add(character)
-    return char_set
-def count_characters(string):
-    char_count = []
-    char_set = characters(string)
-    for char in char_set:
-        count = string.count(char)
-        char_count.append(char + ':')
-        char_count.append(count)
-    return char_count
-
-
-
-#my_str = 'The harder I work, the luckier I get.'
-#print(count(my_str))
-#print(characters(my_str))
-#print(count_characters(my_str))
-print(121**562 % 7)
