85985da61c11ad8100d1ce29c3bf3faf1018a760
Caelan Osman <caelano@byu.edu>
2020-11-16 19:17:43 -0700

    helping Nate

--- _Homework/ideas.py
+++ _Homework/ideas.py
@@ -1,12 +1,24 @@
 import numpy as np
+from numpy import linalg as la
+from scipy import linalg as spla
 
-A = np.array([[1.1, -0.5, 1, 0],
-              [3, 1/4., 1/5., 1/6.],
-              [1/3., 1/10, 1/20., 1/16.],
-              [-3, -0.11, 8, 0]])
-print(A)
+def compact_svd(A, tol=1e-6):
+    eigs, V = la.eig(A.conj().T @ A)
+    singular_vals = np.sqrt(abs(eigs))
+    idx = np.argsort(singular_vals)[::-1]
+    sigma = singular_vals[idx]
+    V1 = V[idx]
 
-print()
-print(np.where(np.where(A > 1, 1, A) < 0, 0, np.where(A > 1, 1, A)))
-print()
-print(np.clip(A, 0, 1))
+
+    return sigma, V1.conj().T
+
+
+A = np.random.random((8,16))
+
+u,s, vh = la.svd(A, full_matrices=False)
+
+s1, v1 = compact_svd(A)
+
+print(s)
+
+print(s1)
