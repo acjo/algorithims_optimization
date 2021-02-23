015fada5a96a2a33e378e6206d112bdac110af6b
Caelan Osman <caelano@byu.edu>
2021-02-23 00:23:05 -0700

    problem 2 finished

--- GradientMethods/gradient_methods.py
+++ GradientMethods/gradient_methods.py
@@ -1,10 +1,13 @@
 # gradient_methods.py
 """Volume 2: Gradient Descent Methods.
-<Name>
-<Class>
-<Date>
+Caelan Osman
+Math 323 Sec. 2
+Feb 22, 2021
 """
 
+import numpy as np
+from scipy import linalg as la
+from scipy.optimize import minimize_scalar as ms
 
 # Problem 1
 def steepest_descent(f, Df, x0, tol=1e-5, maxiter=100):
@@ -24,7 +27,27 @@ def steepest_descent(f, Df, x0, tol=1e-5, maxiter=100):
         (bool): Whether or not the algorithm converged.
         (int): The number of iterations computed.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+
+    #set loop coutner
+    i = 0
+    converge = False
+    while i < maxiter:
+        #set function
+        phi = lambda alpha: f(x0 - alpha* Df(x0).T)
+        #comptue optimal alpha with 1d optimization
+        #compute 1d optimization
+        alpha = ms(phi).x
+        #get next iteration
+        x0 = x0 - alpha*Df(x0).T
+        #iterate loop counter
+        i += 1
+        #check convergence
+        if la.norm(Df(x0), ord=np.inf) < tol:
+            converge = True
+            break
+
+    #return approx min, convergence, number of iterations
+    return x0, converge, i
 
 
 # Problem 2
@@ -42,7 +65,37 @@ def conjugate_gradient(Q, b, x0, tol=1e-4):
         (bool): Whether or not the algorithm converged.
         (int): The number of iterations computed.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    #set intial r values copy because arrays are immutable
+    r0 = Q@x0 - b
+    r1 = np.copy(r0)
+    #set intial conjugate gradient direction
+    d0 = -np.copy(r0)
+    #loop counter, mixiter, and convergence bool
+    i, maxiter = 0, x0.size
+    converge = False
+
+    while i < maxiter:
+        #iterate alpha
+        alpha = np.inner(r0, r0) / np.inner(d0, Q @ d0)
+        #iterate x
+        x0 = x0 + alpha * d0
+        #iterate r
+        r1 = r0 + alpha * Q @ d0
+        #iterate beta
+        beta = np.inner(r1, r1) / np.inner(r0, r0)
+        #iterate d
+        d0 = - r1 + beta * d0
+        #reassign r0
+        r0 = r1
+        #iterate loop counter
+        i += 1
+        #check convergence
+        if la.norm(r0) < tol:
+            converge = True
+            break
+
+    return x0, converge, i
+
 
 
 # Problem 3
@@ -115,3 +168,46 @@ def prob6(filename="challenger.npy", guess=np.array([20., -1.])):
                         Defaults to [20., -1.]
     """
     raise NotImplementedError("Problem 6 Incomplete")
+
+if __name__ == "__main__":
+
+    #problem 1
+    #test 1
+    '''
+    #f(x,y,z) =x^4 + y^4 + z^4
+    fx = lambda x: x[0]**4 + x[1]**4 + x[2]**4
+    Df = lambda x: np.array([4*x[0]**3, 3*x[1]**3, 4*x[2]**3])
+    x0 = np.array([2, -3, 6])
+    optim = steepest_descent(fx, Df, x0, tol=1e-20, maxiter=20)
+    print(optim)
+    '''
+    #test 2 (rosenbrock)
+    #f(x, y) = (1 - x)^2 + 100(y - x^2)^2
+    '''
+    fx  = lambda x: (1 - x[0])**2 + 100*(x[1] - x[0]**2)**2
+    Df = lambda x: np.array([-2*(1 - x[0]) - 400*x[0]*(x[1] - x[0]**2), 200*(x[1] - x[0]**2)])
+    x0 = np.array([-2, 3])
+    optim = steepest_descent(fx, Df, x0, tol=1e-10, maxiter=10000000)
+    print(optim)
+    '''
+
+    #problem 2
+    '''
+    #test 1
+    Q = np.array([[2, 0], [0, 4]])
+    b = np.array([1, 8])
+    x0 = np.array([13, -4])
+    print(conjugate_gradient(Q, b, x0))
+    #test prob 1
+    fx = lambda x: x[0]**2 + 2*x[1]**2 - x[0] - 8*x[1]
+    Df = lambda x: np.array([2*x[0] - 1, 4*x[1] - 8])
+    x0 = np.array([3, 4])
+    print(steepest_descent(fx, Df, x0))
+    #test 2
+    n=4
+    A = np.random.random((n, n))
+    Q = A.T @ A
+    b, x0 = np.random.random((2, n))
+    print(conjugate_gradient(Q, b, x0))
+    '''
+
