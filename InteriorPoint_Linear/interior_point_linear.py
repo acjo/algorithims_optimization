9f67c412fa3f9d641eac68593d865b2fc4b7cf78
Caelan Osman <caelano@byu.edu>
2021-03-18 02:11:11 -0600

    fix prob 4

--- InteriorPoint_Linear/interior_point_linear.py
+++ InteriorPoint_Linear/interior_point_linear.py
@@ -37,6 +37,7 @@ def starting_point(A, b, c):
     mu += dmu*np.ones_like(mu)
 
     return x, lam, mu
+
 # Use this linear program generator to test your interior point method.
 def randomLP(j,k):
     """Generate a linear program min c^T x s.t. Ax = b, x>=0.
@@ -79,27 +80,130 @@ def interiorPoint(A, b, c, niter=20, tol=1e-16, verbose=False):
         x ((n, ) ndarray): The optimal point.
         val (float): The minimum value of the objective function.
     """
-    #vector valued function for lagrangian
+    m, n = A.shape
+    #n x n zero matrix
+    zero_nn = np.zeros((n, n))
+    #m x n zero matrix
+    zero_mn = np.zeros((m, n))
+    #m x m zero matrix
+    zero_mm = np.zeros((m, m))
+    #n x n identity amtrix
+    I_nn = np.eye(n)
+    block_1 = np.column_stack((zero_nn, A.T, I_nn))
+    block_2 = np.column_stack((A, zero_mm, zero_mn))
+
+    block_1_2 = np.vstack((block_1, block_2))
+
     def F(x, lamb, mu):
-        "designs the vector vaued function used the interiro point problem"
-        "Paramaters:
-             x ((n,) ndarray): variable in objective function
-             lambda ((m, ) ndarray): KKT condition lagrange multiplier
-             mu ((n, ) ndarray): KKT condition lagrange multiplier
-        Returns:
-             output ((2n +m, ) ndaraay) output of function
-        "
+        ''' defines the vector vaued function used the interior point problem
+            Paramaters:
+                x ((n,) ndarray): variable in objective function
+                lambda ((m, ) ndarray): KKT condition lagrange multiplier
+                mu ((n, ) ndarray): KKT condition lagrange multiplier
+            Returns:
+                ((2n +m, ) ndaraay) output of function
+        '''
         return np.array([A.T @ lamb + mu - c, A @ x - b, np.diag(mu) @ x])
 
-    #vector valued function for derivative of lagrangian
-    def DF(x, lamb, mu):
+    def block3(x, mu):
+        '''Returns the last block row in the block matrix defining the
+           Ferchet derivative.
+            Paramaters:
+                x ((n,) ndarray): variable in objective function
+                mu ((n, ) ndarray): KKT condition lagrange multiplier
+            Returns:
+                ((n,n2n+m) ndaraay) output of function
+
+        '''
+        M = np.diag(mu)
+        X = np.diag(x)
+        m, n = A.shape
+        #n x m zero matrix
+        zero_nm = np.zeros((n, m))
+        #return bottom block
+        return np.column_stack((M, zero_nm, X))
 
     #search direction subtroutine
-    def direction(sigma=0.1):
+    def direction(x, lamb, mu, nu, sigma=0.1):
+        '''searching direction subroutine
+           Paramaters:
+               sigma (float) defailting to 0.1 used for scaling
+            Returns:
+                delta_x ((n, ) ndarray) delta x
+                delta_lambda ((m, ) ndarray) delta lambda
+                delta_mu ((n, ) ndarray) delta mu
+        '''
+        block_3 = block3(x, mu)
+        DF = np.vstack((block_1_2, block_3))
 
+        center = np.concatenate((np.zeros(n), np.zeros(m), sigma*nu*np.ones(n)))
+        print(F(x, lamb, mu).size)
+        solution = la.lu_solve(la.lu_factor(DF), -F(x, lamb, mu) + center)
 
+        delta_x, delta_lamb, delta_mu = solution[:n], solution[n:n+m], solution[n+m:]
+
+        return delta_x, delta_lamb, delta_mu
+
+    def step_size(x, delta_x, mu, delta_mu):
+        ''' Computes the step length for each iteration:
+            Paramaters:
+                x ((n, ) ndarray) current x
+                d_x ((n, ) ndarray) change in current x
+                mu ((n, ) ndarray) current lagrange mulgiplier
+                d_mu ((n, ) ndarray) change incurrent lagrange mulgiplier
+            Returns:
+                alpha (float) step length for x
+                delta (float) step length for mu
+        '''
+        mask_1 = delta_x < 0
+        mask_2 = delta_mu < 0
+
+        possible_deltas = -1/x[mask_1] * delta_x[mask_1]
+        possible_alphas = -1/mu[mask_2] * delta_mu[mask_2]
+
+        if possible_deltas.size == 0:
+            delta = 1
+        else:
+            delta = np.min(possible_deltas)
+        if possible_alphas.size == 0:
+            alpha = 1
+        else:
+            alpha = np.min(possible_alphas)
+
+        return delta, alpha
+
+    #get intial point
+    x, lam, mu = starting_point(A, b, c)
+    i = 0
+    nu = np.inner(x, mu) / n
+    if abs(nu) < tol:
+        return x, np.inner(c, x)
+    while i < niter:
+        #get directions
+        direct_x, direct_lam, direct_mu = direction(x, lam, mu, nu, sigma=0.1)
+        #get step sizes
+        alph, delt = step_size(x, direct_x, mu, direct_mu)
+        #get new x, lambda, and mu values
+        x += delt * direct_x
+        lam += alph * direct_lam
+        mu += alph * direct_mu
+        #update iteration count
+        i += 1
+        #get new duality measure
+        nu = np.inner(x, mu) / n
+        #check convergence
+        if abs(nu) < tol:
+            return x, np.inner(c, x)
 
 
 def leastAbsoluteDeviations(filename='simdata.txt'):
     """Generate and show the plot requested in the lab."""
     raise NotImplementedError("Problem 5 Incomplete")
+
+
+if __name__ == "__main__":
+
+    j, k = 7, 5
+    A, b, c, x = randomLP(j, k)
+    point, value = interiorPoint(A, b, c)
+    print(np.allclose(x, point[:k]))
