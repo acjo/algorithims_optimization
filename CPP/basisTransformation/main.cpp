7bf6d4e8c03ca1e54edbd816baad8b5cc60f9438
Caelan Osman <caelan@coreform.com>
2021-05-18 12:25:53 -0600

    testing

--- CPP/basisTransformation/main.cpp
+++ CPP/basisTransformation/main.cpp
@@ -7,15 +7,37 @@
 
 using namespace std;
 
+double metric( const vector<double>& vec1, const vector<double>& vec2, int norm= 2)
+{
+	const int largest = max(vec1.size(), vec2.size());
+
+	double result = 0;
+
+	for(auto i = 0; i < largest; ++i)
+	{
+		if (i >= vec1.size())
+			result += pow(0 - vec2.at(i), norm);
+
+		else if( i >= vec2.size())
+			result += pow(vec1.at(i) - 0, norm);
+
+		else
+			result += pow(vec1.at(i) - vec2.at(i), norm);
+	}
+
+	return pow(result, 1.0/norm); 
+}
+
 
 int main( int argc, char* argv[] )
 {
-	auto const num = fabs(3- 2);
 
+	vector<double> first = {1, 2, 3, 7};
+	vector<double> second = {4, 5, 6};
 
-	int num1, int num2, int num3 = 1, 2, 3;
+	double distance = metric(first, second);
 
-
+	cout << "distance: " << distance << endl;
 
 
 	return 0;
